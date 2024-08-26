3afce0f81b9c78837cab4181607733b37cca0814
Dany.Makhoul <dany.makhoul@dawsoncollege.qc.ca>
2024-08-26 16:53:37 -0400

    clean up code related to clerk

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -2,24 +2,24 @@ import Header from '@/components/shared/Header'
 import React from 'react'
 import { transformationTypes } from '@/constants'
 import TransformationForm from '@/components/shared/TransformationForm';
-import { auth } from '@clerk/nextjs/server';
+// import { auth } from '@clerk/nextjs/server';
 import { getUserById } from '@/lib/actions/user.actions';
 import { redirect } from 'next/navigation';
 
 const AddTransoformationTypePage = async({ params: {type}} : SearchParamProps) => {
-  const { userId } = auth();
-  const transformation = transformationTypes[type];
+  // const { userId } = auth();
+  // const transformation = transformationTypes[type];
 
-  if(!userId) redirect('/sign-in');
+  // if(!userId) redirect('/sign-in');
   
-  const user = await getUserById(userId);
+  // const user = await getUserById(userId);
   return (
     <>
-    <Header 
+    {/* <Header 
     title={transformation.title}
     subtitle={transformation.subTitle}
     />
-    <TransformationForm action="Add" userId={user._id} type={transformation.type as TransformationTypeKey} creditBalance={user.creditBalance}/>
+    <TransformationForm action="Add" userId={user._id} type={transformation.type as TransformationTypeKey} creditBalance={user.creditBalance}/> */}
     </>
     
   )
