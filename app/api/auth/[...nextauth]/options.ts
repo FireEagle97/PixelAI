ca4ea899d1181ead9b4f3a99aa2730d0ebb501ca
Dany.Makhoul <dany.makhoul@dawsoncollege.qc.ca>
2024-08-27 16:41:14 -0400

    add check null for github

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -2,12 +2,13 @@ import type { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github"
 import CredentialsProvider from "next-auth/providers/credentials"
 export const options: NextAuthOptions = {
+
     providers: [
         GitHubProvider({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET_ID as string,
+            clientId: process.env.GITHUB_ID! as string,
+            clientSecret: process.env.GITHUB_SECRET_ID! as string,
             authorization: { params: { scope: 'read:user user:email' } },
-                
+
         }),
         CredentialsProvider({
             name: "Credentials",
@@ -21,16 +22,19 @@ export const options: NextAuthOptions = {
                     label: "Password",
                     type: "password",
                     placeholder: "Your password"
-                }  
+                }
             },
-            async authorize(credentials){
+            async authorize(credentials) {
                 // this is where you need to retrieve user data
                 // to verify with credentials
                 // Docs: https://next-auth.js.or/configuration/providers/credentials
+                console.log('GitHub Client ID:', process.env.NEXTAUTH_URL)
+                console.log('GitHub Client Secret:', process.env.GITHUB_SECRET)
+
                 const testuser = { id: "42", name: "Dave", password: "nextauth" }
-                if(credentials?.username === testuser.name && credentials?.password == testuser.password){
+                if (credentials?.username === testuser.name && credentials?.password == testuser.password) {
                     return testuser
-                }else{
+                } else {
                     return null
                 }
             }
