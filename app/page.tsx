5537df91d6f265dd45862afe35db01f4ad9e8f53
Dany.Makhoul <dany.makhoul@dawsoncollege.qc.ca>
2024-08-27 15:58:27 -0400

    remove children from root page

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,21 @@ import React from 'react'
 import { options } from "./api/auth/[...nextauth]/options"
 import { getServerSession } from "next-auth/next"
 import RootLayout from './layout'
-async function Home({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+async function Home(){
   //example of protecting home page
   const session = await getServerSession(options)
   return (
-    <>
-    {session ? (
-      <RootLayout>{children}</RootLayout>
-    ) : (
-      <h1 className="text-5xl">You shall not pass!</h1> 
-    )}
-      </>
+    <div>
+    <h1 className="text-3xl font-bold">Welcome to the Homepage!</h1>
+    <p>This is the main content of the homepage.</p>
+  </div>
+    // <>
+    // {session ? (
+    //   <RootLayout>{children}</RootLayout>
+    // ) : (
+    //   <h1 className="text-5xl">You shall not pass!</h1> 
+    // )}
+    //   </>
 
   )
 }
